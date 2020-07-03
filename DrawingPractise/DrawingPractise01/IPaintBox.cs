8cb0b73f2f71523d080e89e0be69f17a1f7582d9
Mark Lysaght <marklysaght2@eircom.net>
2020-07-03 19:27:14 +0100

    First cut

---
+++ DrawingPractise/DrawingPractise01/IPaintBox.cs
@@ -0,0 +1,16 @@
+﻿using DrawingPractice01.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrawingPractice01
+{
+    public interface IPaintBox
+    {
+        void DrawLine(DrawColour colour, int x, int y);
+
+        void DrawRectangle(DrawColour colour, int left, int top, int right, int bottom);
+
+        void DrawCircle(DrawColour colour, int centreX, int centreY, int radius);
+    }
+}
---
+++ DrawingPractise/DrawingPractise01/Infrastructure/DrawColour.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrawingPractice01.Infrastructure
+{
+    public enum DrawColour
+    {
+        Black,
+        White,
+        Red,
+        Blue,
+        Green,
+        Brown,
+        Yellow
+    }
+}
